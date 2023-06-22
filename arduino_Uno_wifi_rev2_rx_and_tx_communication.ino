bc5dc879375d97bfa8066fc0f230298580b4a9b8
ajettesla <56906721+ajettesla@users.noreply.github.com>
2023-06-22 15:08:33 +0530

    Update arduino_Uno_wifi_rev2_rx_and_tx_communication.ino

--- arduino_Uno_wifi_rev2_rx_and_tx_communication.ino
+++ arduino_Uno_wifi_rev2_rx_and_tx_communication.ino
@@ -3,15 +3,30 @@ SoftwareSerial softSerial(10, 11);
 char ip;
 String ip_full="";
 int count = 0;
+char message[]="hello";
+
 void setup()  
 { 
   Serial.begin(9600);
+  
   softSerial.begin(9600);
 } 
 void loop()  
 { 
   delay(100);
-  softSerial.write('+');
+  
+  // for (int i = 0; i < strlen(message); i++) {
+  //   // Perform encoding-specific operations on the character
+  //   // For example, convert from ASCII to UTF-8
+
+  //   // Example: UTF-8 encoding for ASCII characters
+  //   byte utf8Char = message[i];  // No actual encoding, as ASCII is a subset of UTF-8
+
+  //   // Send the encoded character
+  //   Serial.write(utf8Char);
+  // }
+
+
   String a = get_pitch_for();
   Serial.println(a);
   //delay(500);
@@ -40,19 +55,19 @@ void loop()
   // Serial.println(count);
 
 String get_pitch_for(){
-  
+  //Serial.println("entry");
   while(true){
     if (softSerial.available())
   {
-     ip=softSerial.read();
+    ip=softSerial.read();
     if(ip == '&'){
-       Serial.println(ip_full);
+       //Serial.println(ip_full);
        String tmp = ip_full;
        ip_full = "";
        return tmp;
     }
     else if(ip != '&') {
-      
+      //Serial.println(ip_full);
       ip_full = ip_full + ip;
     }
    
